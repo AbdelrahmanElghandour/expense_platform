c9914dc3f21574fbe9879f505d93309843ed9661
Abdelrahman Elghandour <abdelrahman.elghandour912@gmail.com>
2026-09-04 17:40:05 +0900

    Updated the database schema

--- backend/src/app.js
+++ backend/src/app.js
@@ -36,9 +36,9 @@ const authLimiter = rateLimit({
     }
 });
 
-// if (process.env.NODE_ENV !== "test") {
-//     app.use("/auth", authLimiter);
-// }
+if (process.env.NODE_ENV !== "test") {
+    app.use("/auth", authLimiter);
+}
 
 app.use("/auth", authRouter);
 
